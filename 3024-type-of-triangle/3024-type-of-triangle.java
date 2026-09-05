a966c013b6b88b145c5a23590c22380b72e84dd6
SRAVANAM THANVEESH <thanveesh2021@gmail.com>
2026-09-05 10:42:12 +0530

    Time: 1 ms (57.47%), Space: 43.6 MB (85.69%) - LeetHub

---
+++ 3024-type-of-triangle/3024-type-of-triangle.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String triangleType(int[] nums) {
+        if(nums[0]==nums[1]&&nums[1]==nums[2]&&nums[2]==nums[0]){
+            return "equilateral";
+        }
+        else if(nums[0]!=nums[1]&&nums[1]!=nums[2]&&nums[2]!=nums[0]){
+            if(nums[0]+nums[1]>nums[2]&&nums[1]+nums[2]>nums[0]&&nums[2]+nums[0]>nums[1]){
+                return "scalene";
+            }
+            else{
+                return "none";
+            }
+        }
+        else{
+            if(nums[0]+nums[1]>nums[2]&&nums[1]+nums[2]>nums[0]&&nums[2]+nums[0]>nums[1]){
+                return "isosceles";
+            }
+            else{
+                return "none";
+            }
+        }
+    }
+}
