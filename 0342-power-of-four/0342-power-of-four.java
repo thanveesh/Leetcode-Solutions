dabdf9dc9f8f2a00e463a83a7bf958d296dafaf7
SRAVANAM THANVEESH <thanveesh2021@gmail.com>
2026-09-07 18:34:29 +0530

    Time: 1 ms (90.08%), Space: 42.5 MB (70.16%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,5 +1,12 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        return n>0&&(n&(n-1))==0&&(n&0x55555555)!=0;
+        // return n>0&&(n&(n-1))==0&&(n&0x55555555)!=0;
+        if(n<=0){
+            return false;
+        }
+        while(n%4==0){
+            n/=4;
+        }
+        return n==1;
     }
 }
